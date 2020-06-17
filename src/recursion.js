5dfbd27f56655959a9e1a006796e99bfecfd6a38
Joe Dry <joe.dry@aol.com>
2020-06-17 10:17:24 -0400

    Finished palindrome function

--- src/recursion.js
+++ src/recursion.js
@@ -110,6 +110,14 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+    string = string.toLowerCase();
+    if (string[0] !== string[string.length - 1]) {
+        return false
+    } else if (string.length < 2) {
+        return true;
+    } else {
+        return palindrome(string.slice(1,string.length - 1));
+    }
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the

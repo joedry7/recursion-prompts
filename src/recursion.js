795ef3df22671e6e9fd3a5539ba9a572dc14a52d
Joe Dry <joe.dry@aol.com>
2020-06-17 13:02:21 -0400

    Finished compareStr function

--- src/recursion.js
+++ src/recursion.js
@@ -194,6 +194,14 @@ var gcd = function(x, y) {
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
 var compareStr = function(str1, str2) {
+    if (str1[0] !== str2[0]) {
+        return false;
+    }
+    if (str1.length < 2 && str2.length < 2 && str1 === str2) {
+        return true;
+    } else {
+        return compareStr(str1.slice(1), str2.slice(1));
+    }
 };
 
 // 16. Write a function that accepts a string and creates an array where each letter
